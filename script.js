363b10de21a8003d1196caaeb78027a971ab7114
MuhammadHamzaAslam <hamzaaslam19087@gmail.com>
2024-02-28 22:54:12 +0500

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var age = "I am 14 Years Old"
+alert(age)
+var visit = "You have visit this site only 1 time"
+alert(visit)
+var birthYear ="My Birth Year Is 2010"
+document.write(birthYear+"<br>"+"<br>")
+var x ="Hamza ordered"
+var y =" 5 T-Shirt's"
+var z =" On XYZ Clothing Store"
+document.write(x+y+z)
